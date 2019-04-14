dafeb7b57a2cd4bc477e7cee4089d513d487bf91
Ajeet <40895624+kumarajeet024@users.noreply.github.com>
2019-04-14 23:45:17 +0530

    o of n

--- CB_wave_prints.cpp
+++ CB_wave_prints.cpp
@@ -1,20 +1,32 @@
 //coding blocks array wave prints
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main() {
     int n,arr[n],target;
     cin>>n;
 for(int i=0;i<n;i++) cin>>arr[i];
 cin>>target;
-for(int i=0;i<n-1;i++){
-    int a= target-arr[i];
-    for(int j=i+1;j<n;j++)
-    {
-        if(arr[j]==a){
-        cout<<arr[i]<<" "<<"and"<<" "<<arr[j]<<endl;
-        }
-        else continue;
+sort(arr,arr+n);
+int left=0,right=n-1;
+// for(int i=0;i<n-2;i++){
+//     int a= target-arr[i];
+//     for(int j=i+1;j<n;j++)
+//     {
+//         if(arr[j]==a){
+//         cout<<arr[i]<<" "<<"and"<<" "<<arr[j]<<endl;
+//         }
+//         else continue;
+//     }
+// }
+while(left<right){
+    if((arr[left]+arr[right])==target){
+    cout<<arr[left]<<" "<<"and"<<" "<<arr[right]<<endl;
+        left++;
+        right--;
     }
+ if((arr[left]+arr[right])<target) left++;
+    else right--;
 }
 	return 0;
 }
