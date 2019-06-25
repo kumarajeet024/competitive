9d2981127487b57e379f9f863b4245ac3a0c31e7
Ajeet <40895624+kumarajeet024@users.noreply.github.com>
2019-06-25 23:46:39 +0530

    multiplicative modulo inverse

---
+++ multiplicative_mod_inverse.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define mod 1000000009
+int gcd(int a,int b){
+	if(b==0) return a;
+	return gcd(b,b%a);
+}
+int x,y,GCD;
+void extended_euclids(int a,int b){
+	// base case
+	if(b==0){
+		x=1;
+		y=0;
+		GCD=a;
+		return;
+	}
+	extended_euclids(b,a%b);
+
+	int current_x=y;
+int current_y=x-(a/b)*y;
+x=current_x;
+y=current_y;
+}
+
+int modulo_inverse(int a,int m){
+	extended_euclids(a,m);
+	//return x;
+	return (x+m)%m; //just to return postive value
+}
+
+using namespace std;
+int main(){
+
+ 		ios_base::sync_with_stdio(false);
+ 		cin.tie(NULL);
+ 		#ifndef ONLINE_JUDGE
+ 		freopen("input.txt", "r", stdin);
+ 		freopen("output.txt", "w", stdout);
+ 		freopen("error.txt", "w", stderr);
+ 		#endif
+
+ 		// extended_euclids(18,30);
+ 		// cout<<x<<" "<<y;
+	cout<<modulo_inverse(6,7); 		
+	return 0;
+}
